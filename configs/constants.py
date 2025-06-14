b6b339124a00f53273cd9004ab96002d444f689b
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-06-14 12:26:13 +0100

    commit_72

--- configs/constants.py
+++ configs/constants.py
@@ -3,6 +3,6 @@ MODEL_NAMES = {
     "llm": "microsoft/biogpt",
 }
 MODEL_WEIGHTS = {
-    "biomedclip": "/biomedclip_finetuned.pth", 
-    "biogpt": "/biogpt_finetuned.pth"         
+    "biomedclip": "/content/drive/MyDrive/biomedclip_finetunedtry3.pth", 
+    "biogpt": "/content/drive/MyDrive/biogpt_finetuned1.pth"         
 }
